13654004f88e2db49284d8e657e2eb747952e34e
JasonLiu104 <jason.liu@104.com.tw>
2022-03-27 16:38:37 +0800

    測試cicd

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,8 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 cd dist
+git config --global user.name "JasonLiu104"
+git config --global user.email "jason.liu@104.com.tw"
 git init //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
 git add -A
 git commit -m 'deploy'

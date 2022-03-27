dff27800ca9c129b29a27a972bc8e7a57df875ae
JasonLiu104 <jason.liu@104.com.tw>
2022-03-27 18:10:45 +0800

    測試cicd

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ npm run build
 # 移動至到打包後的dist目錄 
 cd dist
 git init 
+git config --global url."https://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"
 git config --local user.email jason.liu@104.com.tw 
 git config --local user.name JasonLiu104
-git remote add origin  https://JasonLiu104:20220110Abcde104@github.com/JasonLiu104/action-test.git
 git checkout -b gh-pages
 git add --all
 git commit -m "deploy to Github pages"
